b8ca1d378d82508f36b6f718a51611ea43470d39
WebDev-Chad <chadwin.allotey@gmail.com>
2022-04-09 04:37:18 -0400

    update

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,23 @@
-import React, { useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect, useContext } from "react";
+import { StyleSheet, Text, View,TouchableOpacity } from "react-native";
 import { Context } from "./context/BlogContext";
+import { FontAwesome } from "@expo/vector-icons";
 
 const ShowScreen = ({ route, navigation }) => {
   const { state } = useContext(Context);
   const { id } = route.params;
 
   const blogPost = state.find((blogPost) => blogPost.id === id);
+
+//   React.useLayoutEffect(() => {
+//     navigation.setOptions({
+//       headerRight: () => (
+//         <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+//           <FontAwesome name="pencil" size={30} />
+//         </TouchableOpacity>
+//       ),
+//     });
+//   }, [navigation]);
 
   return (
     <View>
@@ -15,8 +26,6 @@ const ShowScreen = ({ route, navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
 
 export default ShowScreen;
